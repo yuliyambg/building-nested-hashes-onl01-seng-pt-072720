52e8b36b530d71bcddbc36304906f5a52554147c
secure-revert-8990 <yuliyambg@gmail.com>
2020-08-02 13:44:23 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,7 +1,6 @@
 def fourth_challenge
   
-  # epic_tragedy[:montague][:hero][:status] = "dead"
-  # epic_tragedy[:capulet][:heroine][:status] = "dead" 
+  
   
   epic_tragedy = {
    :montague => {
@@ -24,6 +23,8 @@ def fourth_challenge
    }
 }
 
+epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead" 
   
 
 end
